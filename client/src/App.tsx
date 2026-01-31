2f8da9c81a4523dfda392f14ae8bdc5092c78f0d
gauravtak <gauravtak996@gmail.com>
2026-01-31 14:07:07 +0530

    punk: setup react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router";
+import Home from "./pages/home";
+import Register from "./pages/register";
+import Login from "./pages/login";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello Everyone</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/signup" element={<Register />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-red-300">
+      <h1>This is home page</h1>
+    </div>
+  );
+}
